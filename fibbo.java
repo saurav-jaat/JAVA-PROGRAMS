671e067f40258ee96cbbbd040839568449bf3bd0
saurav chaudhary <55060238+saurav-jaat@users.noreply.github.com>
2020-03-23 10:23:07 +0530

    Update fibbo.java

--- fibbo.java
+++ fibbo.java
@@ -1,3 +1,7 @@
+/*This program is related to the generation of Fibonacci numbers.
+
+For example: 0, 1, 1, 2, 3, 5, 8, 13,… is a Fibonacci sequence where 13 is the 8thFibonacci number.
+A partial code is given and you have to complete the code as per the instruction given as comments.*/
 import java.util.Scanner;
 class fibbo
 {
